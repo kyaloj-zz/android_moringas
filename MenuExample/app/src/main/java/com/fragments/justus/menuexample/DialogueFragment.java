731fbeb8e65adbf00107a835aaac0f9f1a7b1a46
justus <jmbaluka@mfarm.co.ke>
2015-04-21 14:33:08 +0300

    Added Menu example project. This shows how to implement menu items in action bar

---
+++ MenuExample/app/src/main/java/com/fragments/justus/menuexample/DialogueFragment.java
@@ -0,0 +1,18 @@
+package com.fragments.justus.menuexample;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+
+/**
+ * Created by justus on 4/9/15.
+ */
+public class DialogueFragment extends DialogFragment{
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
